7eeeabcb928e947052e71efe6502de10a62b5000
Nadya Mazko <nadya.mazko.04@mail.ru>
2025-06-01 22:23:01 +0300

    fix PEP8 problems

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -11,15 +11,20 @@ router.register(r'groups', GroupViewSet, basename='groups')
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/api-token-auth/', obtain_auth_token),
-    path('v1/posts/<int:post_id>/comments/', 
-         CommentViewSet.as_view({'get': 'list', 'post': 'create'})),
     path(
-    'v1/posts/<int:post_id>/comments/<int:pk>/',
-    CommentViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    })
-),
+        'v1/posts/<int:post_id>/comments/',
+        CommentViewSet.as_view({
+            'get': 'list',
+            'post': 'create'
+        })
+    ),
+    path(
+        'v1/posts/<int:post_id>/comments/<int:pk>/',
+        CommentViewSet.as_view({
+            'get': 'retrieve',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy'
+        })
+    ),
 ]
